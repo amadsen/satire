e5e37187b0d7b17291708214ae7e23d94be0c827
Aaron Madsen <amadsen@codifiant.com>
2017-08-03 12:44:36 -0600

    Fix the most obvious bugs. Add some mocks. Not yet resolving mocks right.

--- cli/satire.js
+++ cli/satire.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 
 const satire = require('../');
-satire.start({ argv: true });
+const server = satire({ argv: true })
+    .on('listening', (err) => {
+        const {
+            port
+        } = server.address();
+        console.log(`Listening on ${port}`);
+    })
+    .on('request', (req, res) => {
+        const start = process.hrtime();
+        console.log(`Request for ${req.url}`);
+        res
+            .on('close', () => {
+                const [sec, nano] = process.hrtime(start);
+                console.warn(`WARN: Request for ${req.url} was closed after ${sec}s ${nano/1000000}ms`);
+            })
+            .on('finish', () => {
+                const [sec, nano] = process.hrtime(start);
+                console.log(`Completed request for ${req.url} in ${sec}s ${nano/1000000}ms`);
+            });
+    });
